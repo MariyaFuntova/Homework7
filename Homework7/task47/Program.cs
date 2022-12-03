3ebaaaffa9aec69e1a3629866619224969593650
Mariya <m_relaxeena@mail.ru>
2022-12-03 18:25:02 +0400

    task47

---
+++ Homework7/task47/Program.cs
@@ -0,0 +1,28 @@
+﻿double [,] generate2DArray (int lengthRow, int lengthCol, int deviation)
+{
+   double[,] array = new double[lengthRow, lengthCol];
+   for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(-deviation,deviation +1);
+        }
+    }
+    return array;
+}
+
+void print2DArray (double [,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for( int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double [,] array = generate2DArray(3,4,100);
+print2DArray(array);
